34c04f6520abc09da023dc1272d9a24a3f759453
HANSOYE <catchingjay@naver.com>
2022-07-24 15:23:10 +0900

    한소예 프로젝트 #2

--- src/soyeah/Trainer.java
+++ src/soyeah/Trainer.java
@@ -1,7 +1,8 @@
 package MiniProject;
-//import MiniProject.Person;
 
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class Trainer extends Person{
 	private int id;
@@ -13,7 +14,9 @@ public class Trainer extends Person{
 	}
 	
 	//매개변수 받는 생성자
-	public Trainer(int id, String type, String award) {
+	public Trainer(String name, String address,int id, String type, String award) {
+		this.name = name;
+		this.address = address;
 		this.id = id;
 		this.type = type;
 		this.award = award;
@@ -39,41 +42,88 @@ public class Trainer extends Person{
 	}
 	
 	/**
-	 * 부모클래스인 Person의 showMenu 메소드를
+	 * 부모클래스인 Person의 register 메소드를
 	 * 오버라이딩한다.
 	 */
+	
+	ArrayList<Trainer> trainer = new ArrayList<Trainer>();
+	
 	@Override
-	public void showMenu() {
+	public void register() {
 		Scanner sc = new Scanner(System.in);
-		Trainer tn = new Trainer();
+		System.out.println("-------------");
+		System.out.println("트레이너 이름을 입력하세요=>");
+		name = sc.next();
+		System.out.println("-------------");
+		System.out.println("트레이너 주소를 입력하세요=>");
+		address = sc.next();
 		System.out.println("-------------");
 		System.out.println("트레이너 번호를 입력하세요=>");
-		int id = sc.nextInt();
-		tn.setId(id);
+		id = sc.nextInt();
+		
 		System.out.println("-------------");
 		System.out.println("트레이너 전공을 입력하세요=>");
-		String type = sc.next();
-		tn.setType(type);
+		type = sc.next();
+		
 		System.out.println("-------------");
 		System.out.println("트레이너 입상 경력을 입력하세요(개월)=>");
-		String award = sc.next();
-		tn.setAward(award);
+		award = sc.next();
 		System.out.println("-------------");
+		
+		trainer.add(new Trainer(name, address, id, type, award));
+		
+		String content = "-------트레이너-------"+"\n"
+				 +"이름: "+name+"\n"
+				 +"주소: "+address+"\n"
+				 +"번호: "+id+"\n"
+				 +"전공: "+type+"\n"
+				 +"입상 경력: "+award+"\n\n";
+		while(true) {
+
+			System.out.println("-------------");
+			System.out.println("1. 정보 저장");
+			System.out.println("9. 종료");
+
+			System.out.println("--------------");
+			System.out.println("메뉴를 선택해주세요");
+			System.out.println("--------------");
+			int sv = sc.nextInt();
+			switch(sv){
+				case 1:
+					String fileName = "C:\\Users\\A0501560\\git\\multicampus_java\\Begin\\src\\MiniProject\\List.txt";
+					try {
+						FileWriter fw = new FileWriter(fileName, true);
+						fw.write(content);
+						fw.flush();
+						fw.close();
+						System.out.println(fileName+"에 저장 완료!");
+					}catch(IOException ex) {
+						System.out.println("파일 쓰기 중 에러: "+ex.getMessage());
+					}
+					break;
+				case 9:
+					System.out.println("시스템을 종료합니다.");
+					break;
+				default:
+					System.out.println("메뉴 중에서 선택해주세요.");
+					continue;
+			}break;
+		}
 	}
-	
-	
+
 	public void showInfo() {
 		Person ps = new Person();
 		ps.setName(name);
 		ps.setAddress(address);
 		System.out.println("---트레이너 정보---"); //instanceOf로 나중에 바꾸기
-		System.out.println("직원     이름: "+ps.getName());
-		System.out.println("직원     주소: "+ps.getAddress());
+		System.out.println("직원     이름: "+name);
+		System.out.println("직원     주소: "+address);
 		System.out.println("트레이너     번호: "+id);
 		System.out.println("트레이너     이름: "+name);
 		System.out.println("트레이너 등록 기간: "+award);
 	}
-
+		
+	
+}//트레이너////////////
 		
 		
-	}//트레이너////////////
